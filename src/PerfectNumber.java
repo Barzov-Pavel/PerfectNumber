c3738b3ae903f8cf4ab0f17e267532fcf2a6e8fa
Barzov-Pavel <barzov_pavel@mail.ru>
2019-07-02 11:17:17 +0300

    Change return

--- src/PerfectNumber.java
+++ src/PerfectNumber.java
@@ -19,9 +19,6 @@ public class PerfectNumber {
             }
         }
 
-        if (sum == number) {
-            return true;
-        }
-        return false;
+        return sum == number;
     }
 }
